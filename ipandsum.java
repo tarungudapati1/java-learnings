3e4d27a7f27bbea825a9e4312388d9866ef230bb
tarungudapati1 <99164944+tarungudapati1@users.noreply.github.com>
2022-02-24 19:15:11 +0530

    uploaded files

---
+++ ipandsum.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		String name=sc.nextLine();
+		int b=sc.nextInt();
+		int a =sc.nextInt();
+		int x = a+b;
+		System.out.println(x);
+	}
+}

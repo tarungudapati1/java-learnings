fccebbddafb27b05fb731c1e48d15aaf7484f719
tarungudapati1 <99164944+tarungudapati1@users.noreply.github.com>
2022-02-24 19:10:40 +0530

    Create helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,9 @@
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.print("taun is dope\n"); // ; is called terminator
+	
+	    	System.out.print("taun is dope\n");
+	    		System.out.print("taun is dope");
+	}
+}
